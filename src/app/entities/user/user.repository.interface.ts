3f3b07e52a366b3dc627b0c260bd8f589ab76e56
Pedro Costa <103465078+PedroCosta083@users.noreply.github.com>
2023-08-30 13:58:25 -0300

    Feat: adding entity User

--- src/app/entities/user/user.repository.interface.ts
+++ src/app/entities/user/user.repository.interface.ts
@@ -1,5 +1,7 @@
 import User from "./user.entities";
-export interface UserRepository{
-    findById(id : string): Promise<User>;
+export interface UserRepositoryInterface{
+    findById(id : string) : Promise<User>;
+    findByLogin(login : string) : Promise<User>
+    findAllByName(name :string) : Promise<User>
     findAll(): Promise<User[]>;
 }
